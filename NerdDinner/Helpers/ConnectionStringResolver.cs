e861d8bd1606359bcceda38c291a7a6cb14941db
Frédéric Harper <fharper@oocz.net>
2013-04-22 14:14:44 -0400

    First version of NerdDinner for AzureFest Québec on 2011-05-25
    Built with Visual Studio 2010, and Windows Azure SDK

---
+++ NerdDinner/Helpers/ConnectionStringResolver.cs
@@ -0,0 +1,19 @@
+﻿
+namespace NerdDinner.Helpers
+{
+    using System.Configuration;
+    using Microsoft.WindowsAzure.ServiceRuntime;
+
+    public static class ConnectionStringResolver
+    {
+        public static string NerdDinnerEntities
+        {
+            get
+            {
+                return (RoleEnvironment.IsAvailable)
+                           ? RoleEnvironment.GetConfigurationSettingValue("NerdDinnerEntities")
+                           : ConfigurationManager.ConnectionStrings["NerdDinnerEntities"].ConnectionString;
+            }
+        }
+    }
+}
